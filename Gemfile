51f12168ebbbee33c14f4fd64753059e75f4b3d7
Axel Kanakan <okulby@yahoo.com>
2014-10-22 20:23:27 -0700

    Provide an 'Add another file' link that uses Javascript so that users can upload more than one file

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ gem 'coffee-rails', '~> 4.0.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+#gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
@@ -50,3 +50,7 @@ end
 gem 'cancan', '~>1.6.10'
 
 gem 'carrierwave', '0.8.0'
+
+gem 'database_cleaner', '1.0.1'
+
+gem 'selenium-webdriver' , '~> 2.41.0'
--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -51,14 +51,17 @@ feature "Creating Tickets" do
 
 	end
 
-	scenario "Creating a ticket with an attachment" do 
+	scenario "Creating a ticket with an attachment", js: true do 
+		
 		fill_in "Title", with: "Add documentation for blink tag"
 		fill_in "Description", with: "The blink has a speed attribute"
 		
 
 		attach_file "File #1", Rails.root.join("spec/fixtures/speed.txt")
+
+		click_link "Add another file"
+
 		attach_file "File #2", Rails.root.join("spec/fixtures/spin.txt")
-		attach_file "File #3", Rails.root.join("spec/fixtures/gradient.txt")
 		
 
 		click_button "Create Ticket"
@@ -68,7 +71,7 @@ feature "Creating Tickets" do
 		within("#ticket .assets") do 
 			expect(page).to have_content("speed.txt")
 			expect(page).to have_content("spin.txt")
-			expect(page).to have_content("gradient.txt")
+			
 		end
 	end
 
