b58e1510b8ff848810755c1dd5f404dadc9f05f3
Axel Kanakan <okulby@yahoo.com>
2014-10-28 12:43:15 -0700

    When updating a comment's status, also update the ticket's status

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,14 +1,15 @@
 class Ticket < ActiveRecord::Base
-  belongs_to :project
-  belongs_to :user
-  validates :title, presence: true
-  validates :description, presence: true, length: { minimum: 10 }
-
-  has_many :assets
   
+  belongs_to :project
+  belongs_to :state
+  belongs_to :user
+  has_many :assets
+  has_many :comments
+
   accepts_nested_attributes_for :assets
 
-  has_many :comments
+  validates :title, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
 
 
 end
