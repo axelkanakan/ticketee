9de9e8a1668779b3ed4a71262768c5ed6e11fc05
Axel Kanakan <okulby@yahoo.com>
2014-11-01 14:04:32 -0700

    Prettify state styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :find_states
   protect_from_forgery with: :exception
 
   private
@@ -14,6 +15,10 @@ class ApplicationController < ActionController::Base
         flash[:alert] ="You must be an admin to do that."
         redirect_to root_path
       end
+    end
+
+    def find_states
+      @states = State.all
     end
 
   	def require_signin!
